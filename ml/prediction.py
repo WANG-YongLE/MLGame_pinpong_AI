27ad3a0819248446e10b2802e598124f133bc7ae
WANG-YongLE <a2a45a789@gmail.com>
2025-03-31 00:10:42 +0800

    修正邊界判定

--- ml/prediction.py
+++ ml/prediction.py
@@ -39,8 +39,8 @@ class Prediction:
             next_ball_x = 195
             ball_speed_x = -ball_speed_x
 
-        if (((next_ball_y-240)*(ball_y-240)<=0 or (next_ball_y-260)*(ball_y-260)<=0) and ((next_ball_x-new_block)*(ball_x-new_block)<=0 or(next_ball_x-new_block-30)*(ball_x-new_block-30<=0))) :
-            if(time==665) : print("A")
+        if (((next_ball_y-235)*(ball_y-235)<=0 or (next_ball_y-265)*(ball_y-265)<=0) and ((next_ball_x-new_block+5)*(ball_x-new_block+5)<=0 or(next_ball_x-new_block-35)*(ball_x-new_block-35<=0))) :
+            print((ball_x,ball_y),ball_speed_x,ball_speed_y,block,"K",frame)
             a = ball_speed_y / ball_speed_x
             pos_str=(-2,-1)
             pos_row=(-1,-2)
@@ -48,28 +48,63 @@ class Prediction:
                 if(ball_speed[0]>0):
                     b = (ball_y+5) - a * (ball_x+5)
                     pos_str=self.find_intersection_str(a,b,new_block)
+                    if(pos_str[1]>=240 and pos_str[1]<=260+5) :
+                        next_ball_x=new_block-5
+                        ball_speed_x=-ball_speed_x 
+                    pos_row=self.find_intersection_row(a,b,240)
+                    if(pos_row[0]>=new_block and pos_row[0]<=new_block+35) :
+                        next_ball_y=240-5
+                        ball_speed_y=-ball_speed_y
+
                 if(ball_speed[0]<0):
                     b = (ball_y+5) - a * (ball_x)
                     pos_str=self.find_intersection_str(a,b,new_block+30)
-                pos_row=self.find_intersection_row(a,b,240)     
+                    if(pos_str[1]>=240 and pos_str[1]<=260+5) :
+                        next_ball_x=new_block-5
+                        ball_speed_x=-ball_speed_x 
+                    pos_row=self.find_intersection_row(a,b,240)
+                    if(pos_row[0]>=new_block-5 and pos_row[0]<=new_block+30) :
+                        next_ball_y=240-5
+                        ball_speed_y=-ball_speed_y                    
             else :
                 if(ball_speed[0]>0):
                     b = (ball_y) - a * (ball_x+5)
                     pos_str=self.find_intersection_str(a,b,new_block)
+                    if(pos_str[1]>=240-5 and pos_str[1]<=260) :
+                        next_ball_x=new_block-5
+                        ball_speed_x=-ball_speed_x 
+                    pos_row=self.find_intersection_row(a,b,260)
+                    if(pos_row[0]>=new_block and pos_row[0]<=new_block+35) :
+                        next_ball_y=240-5
+                        ball_speed_y=-ball_speed_y   
                 if(ball_speed[0]<0):
                     b=(ball_y)-a*ball_x
                     pos_str=self.find_intersection_str(a,b,new_block+30)
-                pos_row=self.find_intersection_row(a,b,260)
+                    if(pos_str[1]>=240-5 and pos_str[1]<=260) :
+                        next_ball_x=new_block-5
+                        ball_speed_x=-ball_speed_x 
+                    pos_row=self.find_intersection_row(a,b,260)
+                    if(pos_row[0]>=new_block-5 and pos_row[0]<=new_block+30) :
+                        next_ball_y=240-5
+                        ball_speed_y=-ball_speed_y   
+
+
+
+
             if(pos_row[0]>=new_block and pos_row[0]<=new_block+30) :
-                if(ball_speed_y>0) : next_ball_y=240-5
-                else : next_ball_y=260
+                if(ball_speed_y>0) : 
+                    next_ball_y=240-5
+                else : 
+                    next_ball_y=260
                 ball_speed_y=-ball_speed_y
             
             if(pos_str[1]>=240 and pos_str[1]<=260) :
-                if(ball_speed_x>0) :next_ball_x=new_block-5
-                else : next_ball_x=new_block+30
+                if(ball_speed_x>0) :
+                    next_ball_x=new_block-5
+                else : 
+                    next_ball_x=new_block+30
                 ball_speed_x=-ball_speed_x
-            print(ball_speed_x,ball_speed_y)
+            print((next_ball_x,next_ball_y),pos_str,pos_row,ball_speed_x,ball_speed_y,new_block,"S",frame+1)
 
         
         return self.predict((next_ball_x,next_ball_y),(ball_speed_x,ball_speed_y),new_block,blocker_d,frame+1,time+1)  
